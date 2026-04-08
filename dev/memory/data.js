bd8a6cb4e7decb981c7b425ee6456d3c15250273
github-action-benchmark <github@users.noreply.github.com>
2026-04-08 04:41:05 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 31aaf2ea1f24f481ea8307c07ea37d6d7d189688

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775620169978,
+  "lastUpdate": 1775623265930,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -2685,6 +2685,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 40.2,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "31aaf2ea1f24f481ea8307c07ea37d6d7d189688",
+          "message": "Refactor PhpType usage to prefer structured types over strings",
+          "timestamp": "2026-04-08T06:29:06+02:00",
+          "tree_id": "b7deee6ee6e759cdcbb0e20be2ddd124f918230d",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/31aaf2ea1f24f481ea8307c07ea37d6d7d189688"
+        },
+        "date": 1775623265611,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 23.8,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 37.5,
             "unit": "MiB"
           }
         ]
