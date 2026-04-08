52dfb2676e1030fe5511bc236f3792113aa7b143
github-action-benchmark <github@users.noreply.github.com>
2026-04-08 15:14:36 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 278364169302846f581b58608b8e31ee1df24e70

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775655884335,
+  "lastUpdate": 1775661276586,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -2787,6 +2787,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 37.5,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "278364169302846f581b58608b8e31ee1df24e70",
+          "message": "Refactor throws analysis to use PhpType for exception types",
+          "timestamp": "2026-04-08T16:50:25+02:00",
+          "tree_id": "02bfd84b35487d529f05b412301dad66f19d89a6",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/278364169302846f581b58608b8e31ee1df24e70"
+        },
+        "date": 1775661275946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 23.6,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 42.7,
             "unit": "MiB"
           }
         ]
