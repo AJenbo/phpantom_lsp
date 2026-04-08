6f6aa3ccf551934edc71bda080bb1d5585999455
github-action-benchmark <github@users.noreply.github.com>
2026-04-08 01:29:34 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 5568602924b43a6ff6d92d27d4023cecd27b9ec5

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775610032731,
+  "lastUpdate": 1775611774958,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -2515,6 +2515,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 40.7,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "5568602924b43a6ff6d92d27d4023cecd27b9ec5",
+          "message": "Preserve intersection and generic types through resolution",
+          "timestamp": "2026-04-08T03:17:37+02:00",
+          "tree_id": "8d609d991769eed24936728afec31f8138bf3e00",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/5568602924b43a6ff6d92d27d4023cecd27b9ec5"
+        },
+        "date": 1775611774094,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 23.5,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 37.5,
             "unit": "MiB"
           }
         ]
