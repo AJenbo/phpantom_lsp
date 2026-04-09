73f8f29db95ea05583e439d31ecb20f1a977f058
github-action-benchmark <github@users.noreply.github.com>
2026-04-09 03:21:51 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 1e0d4372ccc820a4435aa62326b3cb7809755b4d

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775703038800,
+  "lastUpdate": 1775704911229,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -3059,6 +3059,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 41.3,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "1e0d4372ccc820a4435aa62326b3cb7809755b4d",
+          "message": "Update inlay hints demo to use email parameter in createUser call",
+          "timestamp": "2026-04-09T05:09:24+02:00",
+          "tree_id": "fe57b2ff55a8995bb0c37a0141fc8f3ed54daae6",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/1e0d4372ccc820a4435aa62326b3cb7809755b4d"
+        },
+        "date": 1775704910275,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 24.7,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 42.8,
             "unit": "MiB"
           }
         ]
