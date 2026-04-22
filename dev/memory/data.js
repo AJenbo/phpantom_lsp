da76a0fb5bfebf4a4f5ef8c8fbfcb448ff946eb2
github-action-benchmark <github@users.noreply.github.com>
2026-04-22 11:52:18 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 6d6081e4a2a0f32027625e40f6acbfaa319fe0a0

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776857201094,
+  "lastUpdate": 1776858738630,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -3909,6 +3909,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 45.8,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "6d6081e4a2a0f32027625e40f6acbfaa319fe0a0",
+          "message": "Add Mago diagnostic proxy with quick-fix code actions",
+          "timestamp": "2026-04-22T13:41:25+02:00",
+          "tree_id": "f70ce93c19d970087895359fb69b0d6b9911b2b9",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/6d6081e4a2a0f32027625e40f6acbfaa319fe0a0"
+        },
+        "date": 1776858737603,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 34.4,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 48.7,
             "unit": "MiB"
           }
         ]
