3356b4454fc36bfb620cc1313cb064ac496d75da
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 11:05:03 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 5f1aee692e890ebeee2436ed012902dcece027c5

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777685512625,
+  "lastUpdate": 1777719903494,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -6017,6 +6017,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 46.1,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "5f1aee692e890ebeee2436ed012902dcece027c5",
+          "message": "Patch SPL iterator stubs for generic type propagation",
+          "timestamp": "2026-05-02T12:48:26+02:00",
+          "tree_id": "9c9db80768f8b8b4273d8b4a969049c70e1c1ac1",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/5f1aee692e890ebeee2436ed012902dcece027c5"
+        },
+        "date": 1777719902332,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 39.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 50.2,
             "unit": "MiB"
           }
         ]
