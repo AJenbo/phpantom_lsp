2cb25dcf2da48cdc71b0862f5a59dd8da4933dd1
github-action-benchmark <github@users.noreply.github.com>
2026-05-06 03:30:52 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 87d19f413533d21133753c286105eb82695762e9

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778037857460,
+  "lastUpdate": 1778038252615,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -7474,6 +7474,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_hello_world",
             "value": 37.5,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 51.3,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "87d19f413533d21133753c286105eb82695762e9",
+          "message": "Show hover for Blade echo delimiters as `e()` function",
+          "timestamp": "2026-05-06T05:19:01+02:00",
+          "tree_id": "4844bb02ee168f71b0edff81fc908e0837c4dcec",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/87d19f413533d21133753c286105eb82695762e9"
+        },
+        "date": 1778038251261,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 39.2,
             "unit": "MiB"
           },
           {
