492971d53935ecca12f0d086143a0b616687815c
github-action-benchmark <github@users.noreply.github.com>
2026-05-07 23:57:42 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for ecd7e423fadd174d1cefd1b398483ef86de7a991

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778190216748,
+  "lastUpdate": 1778198262078,
   "repoUrl": "https://github.com/AJenbo/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -8193,6 +8193,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 51.1,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "ecd7e423fadd174d1cefd1b398483ef86de7a991",
+          "message": "Preserve class_index and fqn_index after didClose to fix GTD on closed\nfiles",
+          "timestamp": "2026-05-08T01:45:51+02:00",
+          "tree_id": "fb7b88b786af5b301681ba4e0647330d495c3b3a",
+          "url": "https://github.com/AJenbo/phpantom_lsp/commit/ecd7e423fadd174d1cefd1b398483ef86de7a991"
+        },
+        "date": 1778198260640,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 31.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 49.6,
             "unit": "MiB"
           }
         ]
