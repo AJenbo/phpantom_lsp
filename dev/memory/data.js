2eb63cd7ac0e47de9958bf15b83c1e30c64e24c4
github-action-benchmark <github@users.noreply.github.com>
2026-07-25 01:59:27 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for d4eac1843cdd82150954a9b406a2dbaf5290f25e

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784943197279,
+  "lastUpdate": 1784944767872,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -20477,6 +20477,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 98.3,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "d4eac1843cdd82150954a9b406a2dbaf5290f25e",
+          "message": "Reuse external proxy pattern",
+          "timestamp": "2026-07-25T03:43:14+02:00",
+          "tree_id": "fc6cdab7eee81c471bfe9efdadb7723f3d087104",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/d4eac1843cdd82150954a9b406a2dbaf5290f25e"
+        },
+        "date": 1784944764451,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 48.3,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 96.4,
             "unit": "MiB"
           }
         ]
