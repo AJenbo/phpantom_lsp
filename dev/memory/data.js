18246e6eecb9ded86f9d6bf0f1cb95ca6ef6c7b6
github-action-benchmark <github@users.noreply.github.com>
2026-07-28 17:46:03 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 55dde0b659106ce6b2df1a9fbdd12c39cd60f8f2

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785257945372,
+  "lastUpdate": 1785260763286,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -21973,6 +21973,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 70.6,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shuvro.nsu.cse@gmail.com",
+            "name": "Shuvro Roy",
+            "username": "shuvroroy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "55dde0b659106ce6b2df1a9fbdd12c39cd60f8f2",
+          "message": "Request input key completion from validation rules (#292)",
+          "timestamp": "2026-07-28T19:31:05+02:00",
+          "tree_id": "296b388b147d42451e6d78d49116fe787f12582c",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/55dde0b659106ce6b2df1a9fbdd12c39cd60f8f2"
+        },
+        "date": 1785260760079,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 33.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 71.9,
             "unit": "MiB"
           }
         ]
