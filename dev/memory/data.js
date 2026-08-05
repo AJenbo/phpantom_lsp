66a7ee1934d077cb0422403198814710f6cc58c6
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 23:32:26 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for ef75866d4a0450d5e9d221892135c0cf367b7375

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785972546168,
+  "lastUpdate": 1785972746808,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -14007,6 +14007,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 71.7,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "ef75866d4a0450d5e9d221892135c0cf367b7375",
+          "message": "\"Extract function\" no longer breaks by-reference writes",
+          "timestamp": "2026-08-06T01:16:02+02:00",
+          "tree_id": "e4cd332918453a5c37fbda0df2aae0c7b464b751",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/ef75866d4a0450d5e9d221892135c0cf367b7375"
+        },
+        "date": 1785972742619,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 33.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 72.8,
             "unit": "MiB"
           }
         ]
