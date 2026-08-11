7664dd4dc787c9225a85753fcce6c07398a6ebe4
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 02:17:45 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 8376e3f93a6538ee0693c77c381acec92d1b71c8

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786414210471,
+  "lastUpdate": 1786414665225,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -18325,6 +18325,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 75.6,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "8376e3f93a6538ee0693c77c381acec92d1b71c8",
+          "message": "A completion item in a Blade template no longer edits a position several\nlines below where the cursor sits",
+          "timestamp": "2026-08-11T04:01:57+02:00",
+          "tree_id": "706718c3c744f2ddabb66f1b022699b4abe085b3",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/8376e3f93a6538ee0693c77c381acec92d1b71c8"
+        },
+        "date": 1786414660636,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 34.4,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 78.2,
             "unit": "MiB"
           }
         ]
