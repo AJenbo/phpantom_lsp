9cb6446d969fb94d8b4fd8f6eec9c3b96f6ba40c
github-action-benchmark <github@users.noreply.github.com>
2026-08-12 02:54:01 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 8903dc190bb83ddae9ab3f3f7113dd108837a212

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786500218838,
+  "lastUpdate": 1786503241306,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -18869,6 +18869,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 72.6,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "8903dc190bb83ddae9ab3f3f7113dd108837a212",
+          "message": "A partially-compatible union argument is now reported",
+          "timestamp": "2026-08-12T04:37:14+02:00",
+          "tree_id": "5ce88e719da410d530a6184111e1523f989c1c06",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/8903dc190bb83ddae9ab3f3f7113dd108837a212"
+        },
+        "date": 1786503236141,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 35.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 74.9,
             "unit": "MiB"
           }
         ]
