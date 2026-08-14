f93b40b8b0ce187d90d6aab2c7dda171591817e0
github-action-benchmark <github@users.noreply.github.com>
2026-08-14 16:41:35 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for d8b2de5d926fde09c0a258adf612c1064eec2b82

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786720550966,
+  "lastUpdate": 1786725695168,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -20943,6 +20943,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 72.5,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "d8b2de5d926fde09c0a258adf612c1064eec2b82",
+          "message": "A builder method chained on an Eloquent relation stays on the relation",
+          "timestamp": "2026-08-14T18:24:56+02:00",
+          "tree_id": "f900f65a9e7886269b4f9172efd51595b2fea733",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/d8b2de5d926fde09c0a258adf612c1064eec2b82"
+        },
+        "date": 1786725689374,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 34.6,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 72.2,
             "unit": "MiB"
           }
         ]
