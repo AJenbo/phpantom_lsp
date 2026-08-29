9d11d693400fbee0d64328d18e674eb9fa1b50b5
github-action-benchmark <github@users.noreply.github.com>
2026-08-29 21:57:46 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 716684c3fe0610dd979e092c9543c73e781a11be

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788040647005,
+  "lastUpdate": 1788040666512,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -25839,6 +25839,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 72,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "716684c3fe0610dd979e092c9543c73e781a11be",
+          "message": "Re-testing a call or a property path re-applies what the first test's\nbranch proved",
+          "timestamp": "2026-08-29T23:46:07+02:00",
+          "tree_id": "aaa4de232beaf0fe5ffb8622945405d2401f8b9a",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/716684c3fe0610dd979e092c9543c73e781a11be"
+        },
+        "date": 1788040659214,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 36.7,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 73.2,
             "unit": "MiB"
           }
         ]
